0c9389bcb936c86f85c6ec24c8bbd0812ac8f873
VictorRamirez18 <vramirez100443@ppsc.edu.mx>
2021-05-07 20:51:36 -0500

    chore: modification about wp

--- WhatsappIcon.php
+++ WhatsappIcon.php
@@ -1,3 +1,3 @@
 <div class="whatsapp hvr-pulse-grow aniWhatsapp">
-	<a href="https://wa.me/8123523388?text=Hola,%20quisiera%20consultar%20información%20sobre%20los%20productos%20que%20ofrece." target="_blank"><i class="fab fa-whatsapp-square"></i></a>
+	<a href="https://wa.me/4411162258?text=Hola,%20quisiera%20consultar%20información%20sobre%20los%20productos%20que%20ofrece." target="_blank"><i class="fab fa-whatsapp-square"></i></a>
 </div>
